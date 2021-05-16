032f627a77afc895d55b46f47e6d253519ad588d
pavel <pavel8897@bk.ru>
2021-05-17 00:47:51 +0300

    second commit

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -12,20 +12,28 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=85b668c0&s=matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=85b668c0&s=matrix`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=85b668c0&s=${str}${
+      `https://www.omdbapi.com/?apikey=85b668c0&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
